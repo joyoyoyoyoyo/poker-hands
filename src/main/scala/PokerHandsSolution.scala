7fcc002f99ed0b064233bd534b9c19e438efa396
Angel Ortega <cs.angelortega@gmail.com>
2017-12-19 12:50:59 -0800

    adding in some functional currying

--- src/main/scala/PokerHandsSolution.scala
+++ src/main/scala/PokerHandsSolution.scala
@@ -1,9 +1,12 @@
 import scala.io.Codec
-import scala.util.{Success, Try}
 
 sealed trait Player { def cards: Set[String] }
-final case class Player1(cards: Set[String]) extends Player
-final case class Player2(cards: Set[String]) extends Player
+final case class Player1(cards: Set[String]) extends Player {
+  val increment = 1
+  def win(points: Int)(score: (Int, Int) => Int): Int = score(points, increment)
+}
+final case class Player2(cards: Set[String]) extends Player {
+}
 
 
 object PokerGame {
@@ -14,15 +17,19 @@ object PokerGame {
 }
 
 object PokerHandsSolution extends App {
-  val pokerGames = io.Source.fromFile("p054_poker.txt")(Codec.UTF8)
+  val pokerGames = io.Source.fromResource("p054_poker.txt")(Codec.UTF8)
     .getLines().map(PokerGame(_))
 
-  val results = pokerGames.map(opponents => {
+  val playerOneScore = 0
+  val results = pokerGames.foldLeft(playerOneScore)((isWin, opponents) => {
     opponents match {
       case (Player1(deck1), Player2(deck2)) => {
         deck1
+        val outcome = Player1(deck1).win(isWin)(_ + _)
+        println(deck1.mkString(" ") + " " +"Outcome: " + outcome.toString)
+        outcome
       }
-      case _ => ???
+      case _ => println("eh?"); isWin
     }
   })
 
